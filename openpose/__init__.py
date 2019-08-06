33820e51858205a327838cb6e2a935f4e5316038
ywj <yinw@kth.se>
2019-08-06 02:41:53 +0200

    models

--- openpose/__init__.py
+++ openpose/__init__.py
@@ -1,4 +1,4 @@
 from . import networks
-
+from . import estimations
 __version__ = '0.0.1'
 
---
+++ openpose/estimations/body.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+Author:
+    Wenjie Yin, yinw@kth.se
+    Reference: https://github.com/Hzzone/pytorch-openpose
+"""
+
+import cv2
+import numpy as np
+
+class Body(object):
